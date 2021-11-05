fe6421b077fd13f23285607ffef8301fbea7b11f
Albert David <albert.david@gmail.com>
2021-11-06 00:18:30 +0100

    added /mnt/.settings persistence partition to sdcard and dropbear keays are generated in persistent section for first time boot with sdcard

--- board/ebaz4205/post-image.sh
+++ board/ebaz4205/post-image.sh
@@ -4,4 +4,10 @@ IMAGE_UENV="uEnv.txt"
 IMAGE_FPGA="ebaz4205_top.bin"
 cp "${BOARD_DIR}/${IMAGE_UENV}" "${BINARIES_DIR}"
 cp "${BOARD_DIR}/${IMAGE_FPGA}" "${BINARIES_DIR}"
+
+#create blank settings image of 64MB in ext3 type to be mounted on target at /mnt/.settings from /dev/mmcblk0p4
+#genimage.cfg includes this sttng.ext3 as 4th partition in final sdcard image.
+fallocate -l 64M "${BINARIES_DIR}/sttng.ext3"
+"${HOST_DIR}/usr/sbin/mkfs.ext3" -F -L STTNG "${BINARIES_DIR}/sttng.ext3"
+
 support/scripts/genimage.sh -c "${BOARD_DIR}/genimage.cfg"
