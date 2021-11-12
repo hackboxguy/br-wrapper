3186a6842af969df487b3b2263508ed957e63eac
Albert David <albert.david@gmail.com>
2021-11-12 20:02:59 +0100

    zedboard: included swupdate utility

---
+++ board/zed/gen-sw-update-image.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+UPDATE_IMAGE_NAME=update-image.swu
+
+BOARD_DIR="$( dirname "${0}" )"
+
+cp ${BOARD_DIR}/sw-description ${BINARIES_DIR}
+
+IMG_FILES="sw-description rootfs.ext4.gz"
+
+#cd into binary director
+cd ${BINARIES_DIR}
+
+for f in ${IMG_FILES} ; do
+	echo ${f}
+done | cpio -ovL -H crc > $UPDATE_IMAGE_NAME
+
+#get back previous directory
+cd -
