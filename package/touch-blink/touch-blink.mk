6b35f30c209b5185754b71682a1409a1d63a28d5
hackboxguy <albert.david@gmail.com>
2024-06-15 17:14:45 +0200

    updated touch-blink package source to github

--- package/touch-blink/touch-blink.mk
+++ package/touch-blink/touch-blink.mk
@@ -3,9 +3,9 @@
 ## touch-blink
 #
 ##############################################################
-TOUCHBLINK_VERSION = 0.1
-TOUCHBLINK_SITE_METHOD = local
-TOUCHBLINK_SITE = $(BR2_EXTERNAL_BRWRAPPER_PATH)/package/sources/touch-blink
+TOUCHBLINK_VERSION = 0.0.1
+TOUCHBLINK_SOURCE = $(TOUCHBLINK_VERSION).tar.gz
+TOUCHBLINK_SITE = https://github.com/hackboxguy/touch-blink/archive
 TOUCHBLINK_INSTALL_STAGING = NO
 TOUCHBLINK_INSTALL_TARGET = YES
 TOUCHBLINK_DEPENDENCIES = libevdev
