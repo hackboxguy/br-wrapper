92c57335eb528b09a47642804a444bb10c89e165
hackboxguy <albert.david@gmail.com>
2024-06-16 16:52:02 +0200

    updated touch-blink package to point to latest sources

--- package/touch-blink/touch-blink.mk
+++ package/touch-blink/touch-blink.mk
@@ -3,11 +3,11 @@
 ## touch-blink
 #
 ##############################################################
-TOUCH_BLINK_VERSION = 0.0.1
+TOUCH_BLINK_VERSION = 0.0.2
 TOUCH_BLINK_SOURCE = $(TOUCH_BLINK_VERSION).tar.gz
 TOUCH_BLINK_SITE = https://github.com/hackboxguy/touch-blink/archive
 TOUCH_BLINK_INSTALL_STAGING = NO
 TOUCH_BLINK_INSTALL_TARGET = YES
 TOUCH_BLINK_DEPENDENCIES = libevdev
-TOUCH_BLINK_CONF_OPTS=-DCMAKE_INSTALL_PREFIX="/usr"
+TOUCH_BLINK_CONF_OPTS=-DCMAKE_INSTALL_PREFIX="/"
 $(eval $(cmake-package))
