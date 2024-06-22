7dd86d01fe94fe609049dda2a5df11fce4a4ec71
hackboxguy <albert.david@gmail.com>
2024-06-22 16:23:17 +0200

    updated touch-blink package to point to latest repo code

--- package/touch-blink/touch-blink.mk
+++ package/touch-blink/touch-blink.mk
@@ -3,7 +3,7 @@
 ## touch-blink
 #
 ##############################################################
-TOUCH_BLINK_VERSION = 0.0.3
+TOUCH_BLINK_VERSION = deadf982f6a91b8a42a92e226174f7d33fcd8a06
 TOUCH_BLINK_SOURCE = $(TOUCH_BLINK_VERSION).tar.gz
 TOUCH_BLINK_SITE = https://github.com/hackboxguy/touch-blink/archive
 TOUCH_BLINK_INSTALL_STAGING = NO
