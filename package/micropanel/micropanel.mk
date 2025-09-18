ca942761a4585cd374a7cb47f41b2d952c750b9e
hackboxguy <albert.david@gmail.com>
2025-09-18 09:47:33 +0200

    enabled micropanel and related dependencies for pi4-touch-demo config and added buildroot package for micropanel

---
+++ package/micropanel/micropanel.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# micropanel
+#
+################################################################################
+MICROPANEL_VERSION = main
+MICROPANEL_SITE = https://github.com/hackboxguy/micropanel.git
+MICROPANEL_SITE_METHOD = git
+MICROPANEL_GIT_SUBMODULES = YES
+MICROPANEL_INSTALL_STAGING = NO
+MICROPANEL_INSTALL_TARGET = YES
+MICROPANEL_DEPENDENCIES = i2c-tools libcurl json-for-modern-cpp
+MICROPANEL_CONF_OPTS = \
+	-DCMAKE_INSTALL_PREFIX="/" \
+	-DINSTALL_SCREEN="config-pi-buildroot.json" \
+	-DINSTALL_HELPER_SCRIPTS=ON \
+	-DINSTALL_MEDIA_FILES=ON \
+	-DINSTALL_SYSTEMD_SERVICE=ON \
+	-DSYSTEMD_UNITFILE_ARGS="-i gpio -s /dev/i2c-3"
+$(eval $(cmake-package))
