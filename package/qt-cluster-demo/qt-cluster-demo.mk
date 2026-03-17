effa72f9baa1a5472591eee5ca7e39646f7bc675
hackboxguy <albert.david@gmail.com>
2026-03-17 14:11:49 +0100

    fixed build error of qt-cluster-demo app

--- package/qt-cluster-demo/qt-cluster-demo.mk
+++ package/qt-cluster-demo/qt-cluster-demo.mk
@@ -8,20 +8,8 @@ QT_CLUSTER_DEMO_VERSION = 1.0
 QT_CLUSTER_DEMO_SITE_METHOD = local
 QT_CLUSTER_DEMO_SITE = $(BR2_EXTERNAL_BRWRAPPER_PATH)/package/qt-cluster-demo/src
 QT_CLUSTER_DEMO_DEPENDENCIES = qt5base qt5declarative
+QT_CLUSTER_DEMO_CONF_OPTS = \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_PREFIX=/usr
 
-define QT_CLUSTER_DEMO_CONFIGURE_CMDS
-	(cd $(@D) && $(TARGET_MAKE_ENV) $(HOST_DIR)/bin/cmake \
-		-DCMAKE_INSTALL_PREFIX=/usr \
-		-DCMAKE_BUILD_TYPE=Release \
-		.)
-endef
-
-define QT_CLUSTER_DEMO_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
-endef
-
-define QT_CLUSTER_DEMO_INSTALL_TARGET_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR=$(TARGET_DIR) install
-endef
-
-$(eval $(generic-package))
+$(eval $(cmake-package))
